2a4a017b03a5330f9e7ec581b86305e2e19e1fd6
Brynik1 <nikitaaprel2014@mail.ru>
2025-11-29 02:13:14 +0300

    Update help.py (docker + requirements)

--- help.py
+++ help.py
@@ -4,6 +4,7 @@ import os
 if __name__ == "__main__":
     structure = []
     project_files = []
+    special_files = ['Dockerfile', 'docker-compose.yml', 'requirements.txt']
 
     for root, dirs, files in os.walk('.'):
         dirs[:] = [d for d in dirs if not d.startswith('.') and d not in ['__pycache__', 'venv']]
@@ -11,10 +12,9 @@ if __name__ == "__main__":
         structure.append(f"{indent}{os.path.basename(root)}/")
 
         for file in sorted(files):
-            if file.endswith('.py') and file != 'help.py':
+            if (file.endswith('.py') and file != 'help.py') or file in special_files:
                 structure.append(f"{indent}  {file}")
                 project_files.append(os.path.join(root, file))
-
 
     print("Структура проекта:")
     print("\n".join(structure))
@@ -22,9 +22,12 @@ if __name__ == "__main__":
 
     for filepath in project_files:
         print(f"\nФайл {filepath}:")
-        with open(filepath, 'r', encoding='utf-8') as f:
-            content = f.read()
-            print(content)
-            total_lines += content.count('\n') + 1
+        try:
+            with open(filepath, 'r', encoding='utf-8') as f:
+                content = f.read()
+                print(content)
+                total_lines += content.count('\n') + 1
+        except Exception as e:
+            print(f"Ошибка при чтении файла: {e}")
 
     print(f"СУММАРНОЕ КОЛИЧЕСТВО СТРОК КОДА: {total_lines}")
