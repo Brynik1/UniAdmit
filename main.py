caa40cd4ba4a405d36651161de4802822eb0227b
Brynik1 <nikitaaprel2014@mail.ru>
2025-11-29 03:26:57 +0300

    Update start app

--- main.py
+++ main.py
@@ -7,23 +7,42 @@ from core import config
 from api import run_api
 
 
+
+def prepare_db(db):
+    db.drop_tables()
+    db.create_tables()
+
+def sample_seed(db):
+    prepare_db(db)
+    config.data.mode = 'sample'
+    print(f"Заполнение базы данных в режиме sample")
+    seeder.seed()
+
+def bulk_seed(db):
+    prepare_db(db)
+    config.data.mode = 'bulk'
+    print(f"Заполнение базы данных в режиме bulk")
+    seeder.seed()
+
+
 if __name__ == '__main__':
     args = sys.argv[1:]
 
-    if '--sample' in args or '--bulk' in args:
-        db_manager.drop_tables()
-        db_manager.create_tables()
-        if '-sample' in args:
-            config.data.mode = 'sample'
-        elif '--bulk' in args:
-            config.data.mode = 'bulk'
+    if '--sample' in args:
+        sample_seed(db_manager)
 
-        print(f"Заполнение базы данных в режиме: {config.data.mode}")
-        seeder.seed()
+    elif '--bulk' in args:
+        bulk_seed(db_manager)
 
-    if '--demo' in args:
+    elif '--demo' in args:
+        if not db_manager.check_tables():
+            sample_seed(db_manager)
+
         execute_all_queries()
 
-    if '--api' in args:
+    elif '--api' in args:
+        if not db_manager.check_tables():
+            sample_seed(db_manager)
+
         run_api()
 
